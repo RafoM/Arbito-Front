b1f1fe2d0884a6a460c7f7b677f5981050ddcfa4
RafoM <74451836+RafoM@users.noreply.github.com>
2025-07-06 05:19:04 +0400

    Add i18n setup with language switcher

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import AppRouter from './routes/AppRouter';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { UIProvider } from './contexts/UIContext';
+import { LanguageSwitcher } from './components/common/LanguageSwitcher';
 
 const App = () => {
   return (
     <ThemeProvider>
       <LanguageProvider>
         <UIProvider>
+          <LanguageSwitcher />
           <AppRouter />
         </UIProvider>
       </LanguageProvider>
